749d2332a7eb5bc43ce80a657925a49afa9e7204
Adrian OT <add.devel@gmail.com>
2023-11-02 22:45:49 -0600

    Added certificates to server

--- main.py
+++ main.py
@@ -2,11 +2,12 @@
 
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from src.server  import iniciar
+from src.server  import iniciar, ssl_context
 
 app = iniciar()
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app.run()
+    #app.run()
+    app.run(debug=True, ssl_context=ssl_context)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- src/server.py
+++ src/server.py
@@ -3,15 +3,22 @@ from src.data_example.users import usuarios
 #Routes
 from .routes import DevicesRoutes, UsersRoutes, MerchantRoutes
 from flask import Flask
-aervidor = Flask(__name__)
 
-@aervidor.route('/')
+import os
+servidor = Flask(__name__)
+
+root_dir = os.path.dirname(__file__)
+cert = os.path.join(root_dir, 'utils/server.crt')
+clave_privada = os.path.join(root_dir, 'utils/server.key')
+ssl_context = (cert, clave_privada)
+@servidor.route('/')
 def index():
     return 'Index!'
 def iniciar():
     #Blueprints
-    aervidor.register_blueprint(DevicesRoutes.router, url_prefix='/dispositivos')
-    aervidor.register_blueprint(UsersRoutes.router, url_prefix='/usuarios')
-    aervidor.register_blueprint(MerchantRoutes.router, url_prefix='/mercancias')
+    servidor.register_blueprint(DevicesRoutes.router, url_prefix='/dispositivos')
+    servidor.register_blueprint(UsersRoutes.router, url_prefix='/usuarios')
+    servidor.register_blueprint(MerchantRoutes.router, url_prefix='/mercancias')
     print('Iniciando la aplicación')
-    return aervidor
+    print(root_dir)
+    return servidor
